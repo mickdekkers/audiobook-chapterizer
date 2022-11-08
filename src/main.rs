5c7d3568c6caa3f38ac0ce9e7bcf3c72d57a940f
Mick Dekkers <mickdekkersnl@gmail.com>
2022-11-08 22:22:45 +0100

    Try rodio for more format support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{env, time::Instant};
 
 use arrayvec::ArrayVec;
-use hound::WavReader;
 use itertools::Itertools;
+use rodio::{Decoder, Source};
 use vosk::{Model, Recognizer};
 
 const SAMPLES_BUFFER_SIZE: usize = 8 * 1024; // 8 kb
@@ -16,17 +16,20 @@ fn main() {
     args.next();
 
     let model_path = args.next().expect("A model path was not provided");
-    let wav_path = args
+    let audio_file_path = args
         .next()
-        .expect("A path for the wav file to be read was not provided");
+        .expect("A path for the audio file to be read was not provided");
 
-    let file = std::fs::File::open(wav_path).expect("Could not open file");
-    let buf_reader = std::io::BufReader::with_capacity(SAMPLES_BUFFER_SIZE * 8, file);
-    let mut wav_reader = WavReader::new(buf_reader).expect("Could not create the WAV reader");
+    // FIXME: need to convert to mono
+    let file = std::fs::File::open(audio_file_path).expect("Could not open file");
+    // let buf_reader = std::io::BufReader::with_capacity(SAMPLES_BUFFER_SIZE * 8, file);
+    let source = Decoder::new(file).expect("Could not create the audio reader");
+    let num_channels = source.channels();
+    let sample_rate = source.sample_rate();
 
     let model = Model::new(model_path).expect("Could not create the model");
-    let mut recognizer = Recognizer::new(&model, wav_reader.spec().sample_rate as f32)
-        .expect("Could not create the recognizer");
+    let mut recognizer =
+        Recognizer::new(&model, sample_rate as f32).expect("Could not create the recognizer");
 
     recognizer.set_max_alternatives(0);
     recognizer.set_words(true);
@@ -36,10 +39,13 @@ fn main() {
 
     let mut buffer: ArrayVec<i16, SAMPLES_BUFFER_SIZE> = ArrayVec::new();
     // TODO: is there a faster way to keep reading the samples into a buffer?
-    for chunk in wav_reader.samples().chunks(SAMPLES_BUFFER_SIZE).into_iter() {
-        for sample_result in chunk {
-            let sample = sample_result.expect("Error reading sample");
+
+    let mut total_samples = 0u64;
+    for chunk in source.chunks(SAMPLES_BUFFER_SIZE).into_iter() {
+        for sample in chunk {
+            // let sample = sample_result.expect("Error reading sample");
             buffer.push(sample);
+            total_samples += 1;
         }
 
         if let vosk::DecodingState::Finalized = recognizer.accept_waveform(&buffer) {
@@ -51,7 +57,7 @@ fn main() {
 
     println!("{:#?}", recognizer.final_result().single().unwrap());
     let end_time = Instant::now();
-    let secs_processed = wav_reader.duration() as f32 / wav_reader.spec().sample_rate as f32;
+    let secs_processed = total_samples as f32 / sample_rate as f32 / num_channels as f32;
     let time_elasped = end_time - start_time;
     eprintln!(
         "Processed {:.3} seconds of audio in {:.3} seconds ({:.3}x RT)",
