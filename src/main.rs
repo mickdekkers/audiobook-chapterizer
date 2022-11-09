8fac35689f096c8bfd14f3bb2ba2ae12ec5e8ac3
Mick Dekkers <mickdekkersnl@gmail.com>
2022-11-09 10:50:11 +0100

    Write all results that contain the word "chapter" to JSONL file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
-use std::{env, time::Instant};
+use std::{env, fs::File, sync::mpsc, thread, time::Instant};
 
 use arrayvec::ArrayVec;
 use audiobook_chapterizer::gimme_audio;
 use itertools::Itertools;
-use vosk::{Model, Recognizer};
+use std::io::Write;
+use vosk::{CompleteResult, Model, Recognizer};
 
 const SAMPLES_BUFFER_SIZE: usize = 8 * 1024; // 8 kb
 
 // TODO: find a way to parallelize the workload
 
 // TODO: create function that returns iterator of recognition results
+
+// CompleteResult has borrowed data which relies on a recognizer.
+// We serialize the data before passing it between threads to work around this.
+fn serialize_result(result: CompleteResult) -> String {
+    serde_json::to_string(&result).expect("json serialization should not fail")
+}
 
 fn main() {
     let mut args = env::args();
@@ -33,11 +40,25 @@ fn main() {
     let mut recognizer =
         Recognizer::new(&model, sample_rate as f32).expect("Could not create the recognizer");
 
-    recognizer.set_max_alternatives(0);
+    recognizer.set_max_alternatives(3);
     recognizer.set_words(true);
     recognizer.set_partial_words(false);
 
     let start_time = Instant::now();
+
+    let (tx, rx) = mpsc::channel::<String>();
+
+    let handle = thread::spawn(move || {
+        let mut file = File::create("output.jsonl").expect("failed to create output file");
+        while let Ok(msg) = rx.recv() {
+            if !msg.contains("chapter") {
+                continue;
+            }
+            println!("Writing {} bytes to output file", msg.len());
+            file.write_all((format!("{}\n", msg)).as_bytes())
+                .expect("failed to write buffer to output file");
+        }
+    });
 
     let mut buffer: ArrayVec<i16, SAMPLES_BUFFER_SIZE> = ArrayVec::new();
     // TODO: is there a faster way to keep reading the samples into a buffer?
@@ -51,13 +72,20 @@ fn main() {
         }
 
         if let vosk::DecodingState::Finalized = recognizer.accept_waveform(&buffer) {
-            println!("{:#?}", recognizer.result());
+            tx.send(serialize_result(recognizer.result())).unwrap();
         }
 
         buffer.clear();
     }
 
-    println!("{:#?}", recognizer.final_result().single().unwrap());
+    // TODO: print progress
+
+    tx.send(serialize_result(recognizer.final_result()))
+        .unwrap();
+    drop(tx);
+    handle.join().unwrap();
+
+    // println!("{:#?}", recognizer.final_result().single().unwrap());
     let end_time = Instant::now();
     let secs_processed = total_samples as f32 / sample_rate as f32 / num_channels as f32;
     let time_elasped = end_time - start_time;
