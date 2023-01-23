3989f2b4e08e01eccf02179135b71130a63ebd96
Mick Dekkers <mickdekkersnl@gmail.com>
2023-01-23 20:36:05 +0100

    feat: print estimated time remaining and ETA

--- src/chapterize/mod.rs
+++ src/chapterize/mod.rs
@@ -1,5 +1,3 @@
-use std::{path::Path, time::Duration};
-
 use crate::{
     audio_provider::AudioProvider,
     chapterize::{
@@ -10,8 +8,12 @@ use crate::{
     fixed_vec_deque::FixedVecDeque,
     format_duration,
 };
-
+use arrayvec::ArrayVec;
+use color_eyre::eyre::{self, Context, ContextCompat};
 use crossbeam::channel;
+use itertools::Itertools;
+use std::io::Write;
+use std::path::Path;
 use std::{
     fs::File,
     path::PathBuf,
@@ -20,13 +22,8 @@ use std::{
         Arc,
     },
     thread,
-    time::Instant,
+    time::Duration,
 };
-
-use arrayvec::ArrayVec;
-use color_eyre::eyre::{self, Context, ContextCompat};
-use itertools::Itertools;
-use std::io::Write;
 use vosk::{CompleteResult, CompleteResultMultiple, Model, Recognizer};
 
 mod results_parser;
@@ -42,6 +39,9 @@ const WRITE_POT_MATCH_CONTEXT: usize = 2;
 const POST_CHAPTER_CONTEXT: usize = 30;
 
 const PROGRESS_INTERVAL: Duration = Duration::from_secs(5);
+
+/// Use the average speed factor of the last 30 seconds to calculate the ETA
+const ETA_CALC_WINDOW: usize = 30 / PROGRESS_INTERVAL.as_secs() as usize;
 
 /// This margin is subtracted from the start timestamp of a chapter when output.
 const PRE_CHAPTER_START_MARGIN: Duration = Duration::from_secs(1);
@@ -86,7 +86,7 @@ pub fn chapterize(options: &ChapterizeOptions) -> Result<(), eyre::Error> {
     recognizer.set_words(true);
     recognizer.set_partial_words(false);
 
-    let start_time = Instant::now();
+    let start_time = chrono::Local::now();
 
     let (result_processor_tx, result_processor_rx) = channel::unbounded::<String>();
     let mut matches_file = match &options.matches_file_path {
@@ -190,12 +190,13 @@ pub fn chapterize(options: &ChapterizeOptions) -> Result<(), eyre::Error> {
         parse_result_processor_handle.join().unwrap();
     });
 
+    assert!(ETA_CALC_WINDOW > 0);
     let total_samples = Arc::new(AtomicU64::new(0));
-
     let (progress_reporter_stop_tx, progress_reporter_stop_rx) = channel::unbounded::<()>();
     let total_samples_clone = total_samples.clone();
     let progress_reporter_handle = thread::spawn(move || {
-        let mut last_time = Instant::now();
+        let mut speed_factors: FixedVecDeque<f32> = FixedVecDeque::with_max_len(ETA_CALC_WINDOW);
+        let mut last_time = chrono::Local::now();
         let mut last_samples = 0u64;
         loop {
             // Wait at most PROGRESS_INTERVAL for a stop message
@@ -204,10 +205,10 @@ pub fn chapterize(options: &ChapterizeOptions) -> Result<(), eyre::Error> {
                 Err(err) if matches!(err, channel::RecvTimeoutError::Disconnected) => break,
                 _ => (),
             }
-            let current_time = Instant::now();
+            let current_time = chrono::Local::now();
             let current_samples = total_samples_clone.load(Ordering::SeqCst);
 
-            let time_delta = current_time - last_time;
+            let time_delta = (current_time - last_time).to_std().unwrap();
             let processed_duration =
                 Duration::from_secs_f32(calc_progress_in_secs(current_samples));
             let processed_duration_delta =
@@ -221,16 +222,40 @@ pub fn chapterize(options: &ChapterizeOptions) -> Result<(), eyre::Error> {
                     100.0,
                 )
             });
+            let speed_factor = processed_duration_delta.as_secs_f32() / time_delta.as_secs_f32();
+            speed_factors.push_back(speed_factor);
+
+            let avg_speed_factor =
+                speed_factors.iter().copied().sum::<f32>() / speed_factors.len() as f32;
+
+            let (remaining_wall_time, eta) = match total_duration {
+                Some(total_duration) => {
+                    let remaining_to_process = total_duration - processed_duration;
+                    let remaining_wall_time = Duration::from_secs_f32(
+                        remaining_to_process.as_secs_f32() / avg_speed_factor,
+                    );
+                    let eta = current_time.checked_add_signed(
+                        chrono::Duration::from_std(remaining_wall_time).unwrap(),
+                    );
+                    (Some(remaining_wall_time), eta)
+                }
+                None => (None, None),
+            };
 
             log::info!(
-                "Progress: {} @ {} of {}, speed= {:.2}x",
+                "Progress: {} @ {} of {}\tSpeed: {:.2}x\tTime left: {}\tETA: {})",
                 match progress_percent {
                     Some(pct) => format!("{:05.2}%", pct),
                     None => "??%".into(),
                 },
                 format_duration(&Some(processed_duration)),
                 format_duration(&total_duration),
-                processed_duration_delta.as_secs_f32() / time_delta.as_secs_f32()
+                speed_factor,
+                format_duration(&remaining_wall_time),
+                match eta {
+                    Some(eta) => eta.format("%a %e %b %Y %T").to_string(),
+                    None => "??".into(),
+                }
             );
 
             last_time = current_time;
@@ -275,9 +300,9 @@ pub fn chapterize(options: &ChapterizeOptions) -> Result<(), eyre::Error> {
     result_processor_handle.join().unwrap();
     progress_reporter_handle.join().unwrap();
 
-    let end_time = Instant::now();
+    let end_time = chrono::Local::now();
     let secs_processed = calc_progress_in_secs(total_samples.load(Ordering::SeqCst));
-    let time_elasped = end_time - start_time;
+    let time_elasped = (end_time - start_time).to_std().unwrap();
     log::info!(
         "Processed {:.2} seconds of audio in {:.2} seconds ({:.2}x RT)",
         secs_processed,
